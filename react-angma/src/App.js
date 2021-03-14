f197dbc3a3a9253951fa1e5783e9706c364e5cab
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-14 23:29:47 +0900

    CSS 적용하는 방법은 3가지가 있음. 태그 안에다 작성하는 법, index.css, App.css에 작성하는법 (이건 클래스명이 겹치는 현상이 일어남), App.module.css -> 를 해당 App.js에 불러서 적용하는법 import Styles from ./App.module.css으로 하면 같은 클래스 명이라고 뒤에 해쉬값으로 변경되어 겹칠 일이 없음

--- react-angma/src/App.js
+++ react-angma/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Hello from './component/Hello'
 import WelCome from './component/Welcome';
+import Styles from './App.module.css';
 
 function App() {
 
   return (
     <div className="App">
       <Hello/>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-      <WelCome/>
+      <div className = {Styles.box}>앱</div>
     </div>
   );
 }
--- react-angma/src/component/Hello.js
+++ react-angma/src/component/Hello.js
@@ -10,13 +10,19 @@ export default Hello;
 */
 
 import World from "./World";
+import Styles from "./Hello.module.css"
 
 export default function Hello () {
     return(
-        <>
-            <h1>Hello</h1>
-            <World/>
-            <World/>
+        <>{/* 스타일 주는 방법_1*/}
+            <h1 style={{
+                color : 'red',
+                borderRight : '1px solid #000',
+                marginBottom : '50px',
+                opacity : 0.5
+
+            }}>Hello</h1>
+            <div className = {Styles.box}>헬로</div> {/*Styles를 선언하고 클래스를 객체로 가져옴 */}
         </>
     )
 }
