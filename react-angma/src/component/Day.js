21adc7b025a723612b1f0080125dc937105396eb
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-17 00:13:12 +0900

    페이지 구현

--- react-angma/src/component/Day.js
+++ react-angma/src/component/Day.js
@@ -1,14 +1,14 @@
 import dummy from '../db/data.json'
+import {useParams} from 'react-router-dom'
 
 export default function Day() {
    // dummy.words
-    const day = 1
+    const {day} = useParams()
     const wordList = dummy.word.filter(word => (
-        word.day === day
+        word.day === Number(day)
     ))
-    console.log(wordList)
-
    return <>
+   <h2>Day {day}</h2>
     <table>
         <tbody>
             {wordList.map(word => (
