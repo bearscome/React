634a4c3fcfde502f1ec69c53114da92cbb71d667
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-17 23:59:31 +0900

    더미데이터 삭제 resApi구현

--- react-angma/src/component/DayList.js
+++ react-angma/src/component/DayList.js
@@ -1,15 +1,54 @@
+import { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
-import dummy from '../db/data.json';
 
 export default function DayList() {
-    console.log(dummy)
-    return <ul className ="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}>
-                <Link to={`day/${day.day}`}>
-                     Day {day.day}
-                </Link>
-            </li>
-        ) )}
-    </ul>
+    const [days, setDays] = useState([]);
+
+
+    useEffect(() => { 
+        fetch('http://localhost:3001/days')
+        .then(res => {
+            return res.json()
+        })
+        .then(data=>{
+            setDays(data)
+        })
+    },[]) 
+    
+
+    return(
+        <ul className ="list_day">
+            {days.map(day => (
+                <li key={day.id}>
+                    <Link to={`day/${day.day}`}>
+                        Day {day.day}
+                    </Link>
+                </li>
+            ) )}
+        </ul>
+    )
 }
+
+/*
+    const [days, setDays] = useState([]);
+    const [counte, setCount] = useState(0)
+
+    function onClick() {
+        setCount(counte + 1);
+    }
+
+    function onClick2() {
+        setDays([
+            ...days,
+            {
+                id:Math.random(),
+                day : 1
+            }
+        ])
+    }
+
+    useEffect(() => { //상태 값이 변경되어 다시 랜더링 된 다음에 호춤 됨
+        console.log('countChange')
+    },[]) // 카운트가 변경되었을 때만 useEffect는 가능 -> 의존성 배열
+    //랜더링 직후 한 번만 실행할 때는 빈 배열
+*/
