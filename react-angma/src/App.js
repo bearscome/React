c770a2aa017af77594528173ebdeb46b7b3930ad
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-20 20:15:37 +0900

    영어 단어장 완료.. 하지만 이해 한 것은 아무것도 없다 ㅋㅋㅋㅋ

--- react-angma/src/App.js
+++ react-angma/src/App.js
@@ -1,26 +1,31 @@
-import Day from './component/Day';
-import DayList from './component/DayList';
-import Header from './component/Header';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import EmptyPage from './component/EmptyPage';
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 
 function App() {
-
   return (
     <BrowserRouter>
       <div className="App">
-        <Header/>
+        <Header />
         <Switch>
-          <Route exact path='/'>
-            <DayList/>
+          <Route exact path="/">
+            <DayList />
           </Route>
-          
-          <Route path='/day/:day'>
+          <Route path="/day/:day">
             <Day />
           </Route>
-
+          <Route path="/create_word">
+            <CreateWord />
+          </Route>
+          <Route path="/create_day">
+            <CreateDay />
+          </Route>
           <Route>
-            <EmptyPage/>
+            <EmptyPage />
           </Route>
         </Switch>
       </div>
