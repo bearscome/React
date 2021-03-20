8d27f07515a0cd2568226296282758a69e5d854c
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-21 01:52:28 +0900

    props까지함 class문법으로 진행 됌

--- react-lifeCoding/src/App.js
+++ react-lifeCoding/src/App.js
@@ -1,53 +1,20 @@
 import {Component} from 'react'
 import './App.css';
+import TOC from './component/TOC'
+import Content from './component/content'
+import Subject from './component/Subject'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject />
+        <Subject title="WEB" sub="world wide web!"/>
         <TOC />
-        <Content />
+        <Content title="HTML" desc="HTML is HyperText Markup Language"/>
       </div>
     )
   }
 }
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-          world wide web !
-      </header>
-    )
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language
-      </article>
-    )
-  }
-}
-
 
 export default App;
