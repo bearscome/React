86eace7f3f2e0ca160ca4b97b7f740238f3f6c1d
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-21 12:31:30 +0900

    setstate(), bind(), e.prevntdefault(),

--- react-lifeCoding/src/App.js
+++ react-lifeCoding/src/App.js
@@ -6,12 +6,45 @@ import Subject from './component/Subject'
 
 
 class App extends Component {
+  constructor(prop) {
+    super(prop)
+    this.state = {
+      mode : "welcome",
+      Subject : {
+        title : "WEB",
+        sub : "world wide web!"
+      },
+      welcome : {title : "Welcome", desc : "Hello REACT!!"},
+      contents : [
+        {id: 1, title: "HTML", desc:"HTML is for information" },
+        {id: 2, title: "CSS", desc:"CSS is design" },
+        {id: 3, title: "JavaScript", desc:"JavaScript is for interactive" },
+      ]
+    }
+  }
   render() {
+    var _title, _desc = null
+    if(this.state.mode ==="welcome") {
+      _title = this.state.welcome.title
+      _desc = this.state.welcome.desc
+    } else if(this.state.mode === "read") {
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"/>
-        <TOC />
-        <Content title="HTML" desc="HTML is HyperText Markup Language"/>
+        {/*<Subject title={this.state.Subject.title} sub={this.state.Subject.sub}/>*/}
+        <header>
+          <h1><a href="/" onClick={function(e) {
+            e.preventDefault();
+            this.setState({
+              mode : 'welcome'
+            }) //바로 this.state.mode로 변경하면 리액트는 모름 그래서 this.setState()를 사용해야함 그리고 bind(this)를 해줘야 this 값을 알 수 있음
+          }.bind(this)}>{this.state.Subject.title}</a></h1>
+            {this.state.Subject.sub}
+        </header>
+        <TOC data = {this.state.contents}/>
+        <Content title={_title} desc= {_desc}/>
       </div>
     )
   }
--- react-lifeCoding/src/component/TOC.js
+++ react-lifeCoding/src/component/TOC.js
@@ -2,12 +2,15 @@ import {Component} from 'react'
 
 class TOC extends Component {
     render() {
+      var data = this.props.data;
+      var dataList = []
+      for(let i=0; i < data.length; i++) {
+        dataList.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+      }
       return (
         <nav>
           <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
+            {dataList}
           </ul>
         </nav>
       )
