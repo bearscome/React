ea3aaf5c31a04801416a45488c8adfe9dbc80884
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-23 23:56:36 +0900

    사이트 1강

--- react-site/src/index.js
+++ react-site/src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+const user = {
+   name : "sex",
+   lastname : 'fuck'
+}
+
+
+function fullname(user) {
+  return user.name + '' + user.lastname
+}
+
+const show = fullname(user)
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <h3>hellow {show}</h3>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
