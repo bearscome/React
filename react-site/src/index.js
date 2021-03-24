7d2af45d0239c127f39eef0ce8b4dc0d5d7c4e48
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-24 23:20:43 +0900

    proprs

--- react-site/src/index.js
+++ react-site/src/index.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-
-const user = {
-   name : "sex",
-   lastname : 'fuck'
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author}/>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
 }
 
-
-function fullname(user) {
-  return user.name + '' + user.lastname
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
 }
 
-const show = fullname(user)
+function UserInfo(props) {
+  <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
+        </div>
+      </div>
+}
+
 
 ReactDOM.render(
-  <h3>hellow {show}</h3>,
+  <Comment />,
   document.getElementById('root')
-);
+)
