d07e1f76bd40308ff84ee9d298e2c1b05032e4c1
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-03-31 23:38:21 +0900

    생명주기함수 (마운트), 컨스트럭쳐, 랜더, 컴포넌트 업데이트, 컴포넌트디드마운트
    언마운트 componentWillUnmount

--- react-nomad/src/App.js
+++ react-nomad/src/App.js
@@ -1,60 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
-const foodLike = [
-  {
-    id : 1,
-    name : "kimchi",
-    image : 'https://img.hankyung.com/photo/201910/01.20660060.1.jpg',
-    rating : 5
-  },
-  {
-    id : 2,
-    name : "samgyuopsal",
-    image : 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile30.uf.tistory.com%2Fimage%2F9942B3395A3501C304B135',
-    rating : 4.8
-  },
-  {
-    id : 3,
-    name : "bibimbab",
-    image: 'http://food.chosun.com/site/data/img_dir/2012/05/24/2012052401688_0.jpg',
-    rating : 5
-  },
-  {
-    id : 4,
-    name : "kimbab",
-    image :'https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG',
-    rating : 5
+class App extends React.Component {
+  //마운트 함수들(생명주기) constructor, render, componenetDidUpdate, componentDidMount
+  // 언마운트 componentWillUnmount
+  constructor(props) { // 제일 먼저 실행되는 함수
+    super(props)
+    console.log('hello')
   }
-]
 
-function Food({name, image, rating}) {
-  return (
-    <>
-      <h1>{name}</h1>
-      <h4>{rating}</h4>
-      <img src= {image} alt={name}/>
-    </>
-  )
+
+  state = {
+    count : 0
+  }
+
+  add = () => {(
+    this.setState(current => ({
+      count : current.count + 1
+    }))
+  )}
+
+
+  minue = () => {(
+    this.setState(current => ({
+      count : current.count -1
+    }))
+  )}
+
+  componentDidUpdate() { // 셋스테이트가 실행된 후 랜더가 다시 그려지면 업데이트 되는 함수
+    console.log('I just updated')
+    //setState -> render -> componentIpdate
+  }
+
+  componentDidMount() { //랜더 끝나고 실행
+    console.log('component rendered')
+    // constructor ->  render -> componentDidMount
+  }
+
+  componentWillUnmount() { //컴포넌트가 화면을 떠날 때 실행되는 함수
+
+  }
+
+  render() { // 콘스트럭쳐 실행되고 랜더 실행
+    console.log('render')
+    return(
+      <div>
+        <h1>안녕 : {this.state.count}</h1>
+        <button onClick = {this.add}>add</button>
+        <button onClick = {this.minue}>minus</button>
+      </div>
+    )
+  }
 }
-
-
-function App() {
-  return (
-    <div>
-      {foodLike.map(dish => (
-      <Food key={dish.id} name ={dish.name} image={dish.image} rating = {dish.rating}/> 
-      )
-      )}
-    </div>
-  )
-}
-
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-}
-
 export default App;
