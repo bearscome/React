7a16f8dbdf30569ad696baa0809325470ef25ae8
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-04-04 21:05:41 +0900

    map을 이용한 반복문

--- react-kossie/src/App.js
+++ react-kossie/src/App.js
@@ -2,20 +2,24 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
-  const [condition, setCondition] = useState(false)
-  const toggle = () => setCondition(!condition)
-  useEffect(() => {
-    console.log(condition)
-  }, [condition]) 
+  const movies = [
+    { title: 'hysoung Coder1', year : 2001, id :1 },
+    { title: 'hysoung Coder2', year : 2002, id :2 },
+    { title: 'hysoung Coder3', year : 2003, id :3 },
+  ]
 
-  const renderConditon = condition ? 'true' : 'false'
+  const renderMovies = movies.map(movie => {
+    return (
+      <div className = "movie" key = {movie.id}>
+        <div className = "movie-title" >{movie.title}</div>
+        <div className = "movie-year" >{movie.year}</div>
+      </div>
+    )
+  });
   return (
     <div className="App">
-      <h1>효성아 공부하자!</h1>
-      <div>
-        {renderConditon}
-      </div>
-      <button onClick = {toggle}>Toggle</button>
+      <h1>Movie List</h1>
+      {renderMovies}
     </div>
   );
 }
