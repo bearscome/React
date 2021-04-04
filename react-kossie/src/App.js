0db1b1045ae13fd65aadefae9edd69cb985efa45
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-04-04 20:26:48 +0900

    리액트 조건문에 따라 변하는 컴포넌트

--- react-kossie/src/App.js
+++ react-kossie/src/App.js
@@ -1,21 +1,21 @@
 import React, {useState, useEffect} from 'react'
-
 import './App.css';
-import Counter from './Components/Counter';
 
 function App() {
-  const [buttonName, setButtonName] = useState('클릭')
-  
-  const click = () => {
-    setButtonName('click')
-  }
+  const [condition, setCondition] = useState(false)
+  const toggle = () => setCondition(!condition)
+  useEffect(() => {
+    console.log(condition)
+  }, [condition]) 
+
+  const renderConditon = condition ? 'true' : 'false'
   return (
     <div className="App">
-      <h3>효성아 공부하자!</h3>
-      <Counter click = "click1"/>
-      <Counter click = {buttonName}/>
-      <Counter />
-      <button onClick = {click}>Click</button>
+      <h1>효성아 공부하자!</h1>
+      <div>
+        {renderConditon}
+      </div>
+      <button onClick = {toggle}>Toggle</button>
     </div>
   );
 }
