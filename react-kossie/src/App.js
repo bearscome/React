6eab5e9aced6d35fe0910b8a50e2f7c8e0d1291d
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-04-04 18:41:43 +0900

    컴포넌트 반복

--- react-kossie/src/App.js
+++ react-kossie/src/App.js
@@ -1,31 +1,13 @@
 import React, {useState, useEffect} from 'react'
+
 import './App.css';
+import Counter from './Components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [test, setTest] = useState(0)
-
-  useEffect(() => {
-    console.log(count)
-  },[test, count]) 
-  // [useEffect는 리액트가 재 랜더링 되면 움직이는 함수, 
-  // 그런데 모든 재랜더링에 반응함 그래서 배열에 값을 넣으면 그 값이 재랜더링 될때만 실행]
-
-  useEffect(() => {
-    console.log('처음 랜더링에만 반응')
-  }, [])
-  // 빈 어레이 값을 넣으면 한 번만 실행 됨
-
-  const add = () => {
-    setCount(count + 1)
-  }
-
-
   return (
     <div className="App">
-      <h3>{count}</h3>
-      <button onClick = {add} >더하기</button>
-      <button onClick = {() => {setTest(test + 1)}} >테스트</button>
+      <h3>효성아 공부하자!</h3>
+      <Counter />
     </div>
   );
 }
---
+++ react-kossie/src/Components/Counter.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+function Counter () {
+    const [count, setCount] = useState(0)
+  
+    const increment = () => {
+      setCount(count + 1)
+    }
+  
+    return (
+        <button onClick = {increment} >더하기 {count}</button>
+    );
+}
+
+export default Counter
