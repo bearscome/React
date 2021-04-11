4e583adac423519b666fd0bd98059fe32d2333e3
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-04-11 23:02:35 +0900

    자시컨퍼넌트 부모컨퍼넌트 건들이기

--- react-kossie/src/App.js
+++ react-kossie/src/App.js
@@ -1,22 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState,} from 'react'
 import Movie from './Components/Movie';
+import MovieForm from './Components/MovieForm';
 
 function App() {
-  const movies = [
-    { title: 'hysoung Coder1', year : 2001, id :1 },
-    { title: 'hysoung Coder2', year : 2002, id :2 },
-    { title: 'hysoung Coder3', year : 2003, id :3 },
-  ]
+  const [movies, setmovies] = useState([
+    { title: 'hysoung Coder1', year : 2001, },
+    { title: 'hysoung Coder2', year : 2002,  },
+    { title: 'hysoung Coder3', year : 2003, },
+  ]);
 
   const renderMovies = movies.map(movie => {
     return (
-      <Movie movie = {movie} key = {movie.id}/>
+      <Movie movie = {movie} key = {movie.title}/>
     )
   });
+  const addMovie = (movie) => {
+    setmovies([
+      ...movies,
+      movie
+    ])
+  };
 
+  
   return (
     <div className="App">
       <h1>Movie List</h1>
+      <MovieForm addMovie = {addMovie}/>
       {renderMovies}
     </div>
   );
